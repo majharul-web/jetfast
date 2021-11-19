0c46e7ecc158b7a053b3bd2f3ceb22bf5c12b44b
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-11-20 00:32:07 +0600

    ddddddd

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <h3 className='text-center text-danger'>Start react</h3>
-<p>ghsf</p>
+
 
     </div>
   );
